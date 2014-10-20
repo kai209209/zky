bcdc5000376a17c3e4f79e4f3cdf5ad3d0611c62
zky <361561797@qq.com>
2014-10-20 16:19:57 +0800

    修改登陆后的路径

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,4 +13,12 @@ class ApplicationController < ActionController::Base
       'application'
     end
   end
+
+  def after_sign_in_path_for(resource)
+    user_path
+  end
+
+  def after_sign_out_path_for(resource)
+    root_path
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  layout 'user'
   def show
     
   end
