19aa7c93f0bea8f10eb0c35e3bea3fd0f9c35399
zky <361561797@qq.com>
2014-10-20 15:50:07 +0800

    增加simple_form的相关规则，添加注册controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  # devise_for :users
+  devise_for :users, :controllers => { :registrations => "registrations" }
+  resource :user
 
   root to: 'static#index'
   
