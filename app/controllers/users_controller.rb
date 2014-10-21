4ab35c3a6b1c6006d1c963bc68161aacab046355
zky <361561797@qq.com>
2014-10-21 16:16:34 +0800

    user 增加update action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,18 @@ class UsersController < ApplicationController
   def edit    
   end
 
-  def update    
+  def update 
+    if current_user.update(update_params)
+      redirect_to user_path
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+  def update_params
+    params.require(:user).permit(:name, :age, :city, :github, :skill, :work_experience)
   end
 
   
